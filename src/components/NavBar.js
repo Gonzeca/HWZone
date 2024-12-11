f5932e7fae17be60cdf62efc717a676e9f3377ae
Gonzalo Castro <gonzeca1@outlook.es>
2024-12-10 22:40:07 -0300

    Correccion

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { byPrefixAndName } from '@awesome.me/kit-KIT_CODE/icons'
+
 
 const NavBar = () => {
     const logo = require('../media/hwzone_logo.png');
-    const fai = (icon) => <FontAwesomeIcon icon={byPrefixAndName.fas[icon]} />
 
     return(
         <header>
@@ -11,7 +9,6 @@ const NavBar = () => {
             <nav><a href='#'>Productos</a></nav>
             <nav><a href='#'>Productos</a></nav>
             <nav><a href='#'>Soporte</a></nav>
-            <nav>{fai('fa-shopping-cart')}</nav>
         </header>
     );
 }
